6432c57df40097eb72b12f9c6230118f8708b51b
Janavi Reddy <Janavi Reddy@DESKTOP-73OQVUV>
2022-02-03 13:05:29 +0530

    first project committed

---
+++ 31-01-2022/src/com/javaBasics/example1.java
@@ -0,0 +1,12 @@
+package com.javaBasics;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		// java program to print hello java
+		
+		System.out.println("Hello Java");
+
+	}
+
+}
